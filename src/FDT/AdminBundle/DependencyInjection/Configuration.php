fbd9749d43b6dfd0f1d19afe0230ab936026d82e
dinoweb <lorenzo@dinoweb.net>
2011-06-29 17:17:30 +0000

    Studio della classe config

---
+++ src/FDT/AdminBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace FDT\AdminBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('fdt_admin');
+
+        $this->addPathSection($rootNode);
+        $this->addMenuSection($rootNode);
+
+        return $treeBuilder;
+    }
+
+    private function addPathSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('pathConfig')
+                    ->addDefaultsIfNotSet()
+                    ->canBeUnset()
+                    ->children()
+                        ->arrayNode('paths')
+                            ->requiresAtLeastOneElement ()
+                            ->prototype('variable')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+    
+ 
+    private function addMenuSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('menuConfig')
+                    ->requiresAtLeastOneElement()
+                    ->children()
+                        ->arrayNode('parties')
+                            ->requiresAtLeastOneElement ()
+                            ->prototype('variable')->end()
+                        ->end()
+                    ->end()
+                    ->children()
+                        ->arrayNode('contents')
+                            ->requiresAtLeastOneElement ()
+                            ->prototype('variable')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+}
